9d1609938b732abb47143ce082879af6cebda93c
Juan1720 <fransc1820@gmail.com>
2022-07-05 22:25:38 -0500

    prototipo juan

---
+++ app/src/main/java/com/example/appphysicare/ListarCitasAgendadasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appphysicare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListarCitasAgendadasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_citas_agendadas)
+    }
+}
