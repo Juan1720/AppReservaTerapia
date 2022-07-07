ad5412b1005cbef84e3922b6fbd43eff76c69ecb
antonyp123 <todynon_bryan12@hotmail.com>
2022-07-06 22:07:36 -0500

    mensaje

---
+++ app/src/main/java/com/example/appphysicare/BotonConocenos.kt
@@ -0,0 +1,11 @@
+package com.example.appphysicare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BotonConocenos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boton_conocenos)
+    }
+}
---
+++ app/src/main/java/com/example/appphysicare/ListarCitaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appphysicare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListarCitaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_cita)
+    }
+}
